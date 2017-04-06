16c3e5ab8d3eed73bec2f0c8d92760cad4cd4924
frowhy <frowhy@gmail.com>
2017-04-06 16:56:36 +0800

    修改富文本框默认为普通输入方式

--- resources/views/layouts/editor.blade.php
+++ resources/views/layouts/editor.blade.php
@@ -1,23 +1,19 @@
 {{-- simditor --}}
 <link rel="stylesheet" type="text/css" href="{{ asset('/bower-assets/simditor/styles/simditor.css') }}"/>
-
 <script type="text/javascript" src="{{ asset('/bower-assets/jquery/dist/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/simple-module/lib/module.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/simple-hotkeys/lib/hotkeys.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/simple-uploader/lib/uploader.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/simditor/lib/simditor.js') }}"></script>
-
+{{-- markdown --}}
+<link rel="stylesheet" href="{{ asset('/bower-assets/simditor-markdown/styles/simditor-markdown.css') }}"/>
 <script type="text/javascript" src="{{ asset('/bower-assets/marked/lib/marked.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/to-markdown/dist/to-markdown.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/bower-assets/simditor-markdown/lib/simditor-markdown.js') }}"></script>
-
-<link rel="stylesheet" href="{{ asset('/bower-assets/simditor-markdown/styles/simditor-markdown.css') }}" media="screen"
-      charset="utf-8"/>
-
+{{-- init simditor --}}
 <script>
     var editor = new Simditor({
         textarea: $('#editor'),
-        markdown: true,
         upload: {
             url: '{{ route('upload') }}',
             params: null,
@@ -25,7 +21,6 @@
             connectionCount: 3,
             leaveConfirm: 'Uploading is in progress, are you sure to leave this page?'
         },
-
         toolbar: ['bold', 'italic', 'underline', 'color', '|', 'ol', 'ul', '|', 'image', '|', 'markdown']
     });
 </script>
