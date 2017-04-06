d109cad53d39d09277135d3ebb7d71e5f015ed94
frowhy <frowhy@gmail.com>
2017-04-06 16:54:59 +0800

    修改代码风格

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -18,14 +18,14 @@ class UploadController extends Controller
      */
     public function postIndex(Request $request)
     {
-        if ($request->hasFile('upload_file')) { // 判断文件是否存在
-
-            $file = $request->file('upload_file'); // 获取文件实例
-
-            if ($file->isValid()) { // 判断文件是否有效
-
-                $ext = $file->getClientOriginalExtension(); // 获取文件拓展名
-
+        // 判断文件是否存在
+        if ($request->hasFile('upload_file')) {
+            // 获取文件实例
+            $file = $request->file('upload_file');
+            // 判断文件是否有效
+            if ($file->isValid()) {
+                // 获取文件拓展名
+                $ext = $file->getClientOriginalExtension();
                 switch ($ext) {
                     case 'jpg':
                         break;
@@ -34,38 +34,30 @@ class UploadController extends Controller
                     case 'gif':
                         break;
                     default:
-
                         return response()->json(['success' => false, 'message' => '格式不正确']);
                 }
-
                 $size = $file->getClientSize();
-
-                if ($size > 2097152) { // 判断文件是否大于2mb
-
+                // 判断文件是否大于2mb
+                if ($size > 2097152) {
                     return response()->json(['success' => false, 'message' => '超过大小']);
                 } else {
-
-                    $file_name = md5(time() . md5($file->getClientOriginalName())) . '.' . $ext; // 生成文件名
-                    $save_path = 'upload/' . $file_name; // 定义保存路径
-                    Storage::put( // 文件写入
-                        $save_path,
-                        file_get_contents($file->getRealPath())
-                    );
-
-                    if (! Storage::exists($save_path)) { // 判断文件系统中是否存在
-
+                    // 生成文件名
+                    $file_name = md5(time() . md5($file->getClientOriginalName())) . '.' . $ext;
+                    // 定义保存路径
+                    $save_path = 'upload/' . $file_name;
+                    // 文件写入
+                    Storage::put($save_path, file_get_contents($file->getRealPath()));
+                    // 判断文件系统中是否存在
+                    if (! Storage::exists($save_path)) {
                         return response()->json(['success' => false, 'message' => '保存失败']);
                     } else {
-
                         return response()->json(['success' => true, 'message' => '上传成功', 'file_path' => asset('storage/' . $save_path)]);
                     }
                 }
             } else {
-
                 return response()->json(['success' => false, 'message' => '文件无效']);
             }
         } else {
-
             return response()->json(['success' => false, 'message' => '文件不存在']);
         }
     }
